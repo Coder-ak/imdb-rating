2407d2c44001c9ef38793d06ae69ddaf3ca897d8
Coder_AK <alexcoder@gmail.com>
2025-11-07 17:45:02 +0000

    udpate node & packages

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "imdb-rating",
-      script: "dist/index.js",
+      script: "./dist/index.js",
       watch: false,
       env: {
         NODE_ENV: "production",
